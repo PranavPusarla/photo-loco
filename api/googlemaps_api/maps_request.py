8d6625166e2fdfa51d91a188c8f49a0d8c1136eb
agollakota01 <67496750+agollakota01@users.noreply.github.com>
2020-07-08 15:43:15 -0700

    Update maps_request.py

--- api/googlemaps_api/maps_request.py
+++ api/googlemaps_api/maps_request.py
@@ -25,7 +25,7 @@ def search(place):
     places = gmaps.find_place(input=place, input_type="textquery", fields='geometry')
     print(places)
 '''
-
+'''
 def search(place):
     url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + place + '&inputtype=textquery&fields=name,formatted_address&key=' + API_KEY
     #url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=' + API_KEY
@@ -35,10 +35,23 @@ def search(place):
     formatted_address = json["candidates"][0]["formatted_address"]
     print(formatted_address)
     return formatted_address
+'''
 
-g_hq_lat = gmaps.geocode('501 Twin Peaks Blvd, San Francisco, CA 94114')[0]["geometry"]["location"]["lat"]
-
-g_hq_long = gmaps.geocode('501 Twin Peaks Blvd, San Francisco, CA 94114')[0]["geometry"]["location"]["lng"]
+def place_textsearch(place):
+    places = []
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+    search_query = input("Where to? ")
+    if search_query == "":
+        search_query = place
+    request = requests.get(url + 'query=' + search_query +
+                        '&key=' + API_KEY)
+    #JSON provides other useful info such as lat/long, formatted_address, photos
+    json = request.json()
+    results = json["results"]
+    for place in results:
+        places.append({"name": place["name"], "address": place["formatted_address"], "lat": place["geometry"]["location"]["lat"], "long": place["geometry"]["location"]["lng"]})
+    print(places)
+    return places
 
 def get_lat_long(address):
     lat = gmaps.geocode(address)[0]["geometry"]["location"]["lat"]
@@ -46,7 +59,7 @@ def get_lat_long(address):
     return lat,long
 
 def find_nearby(lat, long):
-    json_result = gmaps.places_nearby(location=str(lat) + ', ' + str(long), radius=20000, type = "tourist_attraction, restaurant")
+    json_result = gmaps.places_nearby(location=str(lat) + ', ' + str(long), radius=20000, type = "hair_care")
     array = []
     result = json_result["results"]
     for location in result:
@@ -55,6 +68,9 @@ def find_nearby(lat, long):
 
 #find_nearby(g_hq_lat, g_hq_long)
 
-address = search("lucalis")
-lat, long = get_lat_long(address)
+place_set = place_textsearch("pizza in Manhattan")
+first_place = place_set[0]
+lat, long = get_lat_long(first_place["address"])
+print()
 find_nearby(lat, long)
+
